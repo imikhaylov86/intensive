708eb2bb1b7f1565fe232cebe3a00c21a9d4fa1e
imikhaylov86 <rogiki@mail.ru>
2020-04-07 15:29:50 +0300

    First Blood!

--- src/day_01/06_sport_tracker.py
+++ src/day_01/06_sport_tracker.py
@@ -2,7 +2,7 @@ ccal = 5000
 step = 340
 step_count = 0
 
-while ccal > 3600:
+while ccal > 3500:
     ccal = ccal - step
     step_count = step_count + 1
     print(f'Шаг {step_count}, каллории {ccal}')
